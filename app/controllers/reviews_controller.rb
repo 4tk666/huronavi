24390af60dbc04960c3702ee46c64840f536d1e1
4tk666 <vhfxhy5356@yahoo.co.jp>
2019-09-20 17:45:53 +0900

    prefecture_idの追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,13 @@
 class ReviewsController < ApplicationController
 
   def index
-  @review = Review.all
+  @review = Review.all.limit(4).order(id: "DESC")
   end
 
   def new
     @review = Review.new
     @review.photos.build
+    @prefecture  = Prefecture.where.not(ancestry: nil)
   end
 
   def create
@@ -23,6 +24,7 @@ class ReviewsController < ApplicationController
       params.require(:review).permit(
         :title,
         :text,
+        :prefecture_id ,
         photos_attributes:[:id,:image]
         ).merge(user_id: current_user.id) 
     end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
+  # validates :text,                 presence: true
+  # validates :title,                presence: true
+  # validates :photos,               presence: true
+  # validates :prefecture_id ,       presence: true
   has_many :photos
   accepts_nested_attributes_for :photos
 end
