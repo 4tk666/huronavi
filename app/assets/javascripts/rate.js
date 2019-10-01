ea0c5e141ca3c9eabcf6a7300ec94e83d4fee76a
4tk666 <vhfxhy5356@yahoo.co.jp>
2019-10-01 18:22:10 +0900

    rateの修正

--- app/assets/javascripts/rate.js
+++ app/assets/javascripts/rate.js
@@ -1,9 +1,9 @@
 $(function(){
   $('#star').raty({
     size     : 36,
-    starOff: '/assets/star-off.png',
-    starOn : '/assets/star-on.png',
-    starHalf: '/assets/star-half.png',
+    starOff: 'https://i.gyazo.com/4c59d744351300e40105a0aaaf4a44ee.png',
+    starOn : 'https://i.gyazo.com/95f15d51ece8113e8d24973d47a57cac.png',
+    starHalf: 'https://i.gyazo.com/7822e4957b99bab445f913b0864f5b7c.png',
     half: true,
     scoreName: 'review[rate]' ,
   });
@@ -11,9 +11,9 @@ $(function(){
   // var score = e.getAttribute('data-review-rate');
   $(".contents-main__list__new__review__rate").raty({
     size     : 36,
-    starOff: '/assets/star-off.png',
-    starOn : '/assets/star-on.png',
-    starHalf: '/assets/star-half.png',
+    starOff: 'https://i.gyazo.com/4c59d744351300e40105a0aaaf4a44ee.png',
+    starOn : 'https://i.gyazo.com/95f15d51ece8113e8d24973d47a57cac.png',
+    starHalf: 'https://i.gyazo.com/7822e4957b99bab445f913b0864f5b7c.png',
     half: true,
     readOnly: true,
     // score: score,
