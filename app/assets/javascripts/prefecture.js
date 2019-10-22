ef3d4def256d5280652e0f6c6869993cae263ffb
4tk666 <vhfxhy5356@yahoo.co.jp>
2019-10-22 16:34:18 +0900

    修正

--- app/assets/javascripts/prefecture.js
+++ app/assets/javascripts/prefecture.js
@@ -1,7 +1,8 @@
 $(function(){
   $("#prefecture").on("mouseover",function(){
     $(".prefecture-list").removeClass("prefecture-url");
-    // $("#prefecture").addClass("prefecture-header");
+    $("#prefecture").addClass("prefecture-header");
+    $(".prefecture-list").addClass("prefecture-header");
   });
   $(".prefecture-url").on("mouseleave",function(){
     $(".prefecture-list").addClass("prefecture-url");
@@ -10,10 +11,17 @@ $(function(){
 $(function(){
   $("#search-icon").on("click",function(){
     $(".nav-header__before").hide();
+    $(".prefecture-list").hide();
     $("#search-form").removeClass("search-form");
   });
   $("#times-icon").on("click",function(){
     $("#search-form").addClass("search-form");
     $(".nav-header__before").show();
+    $(document).on("mouseover","#prefecture",function(){
+      $(".prefecture-list").show();
+    });
+    $(document).on("mouseleave",".prefecture-list",function(){
+      $(".prefecture-list").hide();
+  });
   });
 });
